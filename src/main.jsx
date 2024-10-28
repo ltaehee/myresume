af8f553e2c34a5f4219d76b8302087f7a0a57fa3
이태희 <taehee3992@naver.com>
2024-10-28 16:01:36 +0900

    init project

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import PortfolioPage from "./pages/PortfolioPage";
+import ContactPage from "./pages/ContactPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/portfolio", element: <PortfolioPage /> },
+      { path: "/contact", element: <ContactPage /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
